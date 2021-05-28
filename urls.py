be034c80ba44a16dee3628dc5d0c4c0fc6a43323
andrenascimento1975 <84337398+andrenascimento1975@users.noreply.github.com>
2021-05-28 14:12:02 -0300

    Initial commit

---
+++ urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from .models import Tarefa, Grupos, Sub_Grupos
+from .views import Logar, Registrar, VisualizaGrupo, VisualizaSubGrupo, CriarGrupo, AtualizarGrupo, \
+    ApagarGrupo, MostraSubGrupo, pega_get, pega_get_tarefa, exclui_subgrupo, edita_subgrupo, exclui_tarefa, edita_tarefa
+from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('login/', Logar.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('registrar/', Registrar.as_view(), name='registrar'),
+    path('', VisualizaGrupo.as_view(), name='grupos'),
+    path('subgrupos/<int:pk>', VisualizaSubGrupo.as_view(), name='subgrupos'),
+    path('criar/<int:grupo>/<int:subgrupo_tar_id>/', pega_get_tarefa, name='criar'),
+    path('criar_grupo/', CriarGrupo.as_view(), name='criar_grupo'),
+    path('criar_subgrupo/<int:grupo>/', pega_get, name='criar_subgrupo'),
+    path('atualizar/<int:pk>', edita_tarefa, name='atualizar'),
+    path('atualizar_grupos/<int:pk>', AtualizarGrupo.as_view(), name='atualizar_grupos'),
+    path('atualizar_subgrupos/<int:pk>', edita_subgrupo, name='atualizar_subgrupos'),
+    path('apagar/<int:pk>', exclui_tarefa, name='apagar'),
+    path('apagar_grupo/<int:pk>', ApagarGrupo.as_view(), name='apagar_grupo'),
+    path('apagar_subgrupo/<int:pk>', exclui_subgrupo, name='apagar_subgrupo'),
+    path('mostra_subgrupo/<int:pk>', MostraSubGrupo.as_view(), name='mostra_subgrupo'),
+]
